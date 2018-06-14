be208382e28446670eacda876ffcc1e9860126cc
grendach <dmytro.grendach@nokia.com>
2018-06-14 09:37:39 +0200

    fixing errors in files

--- people.py
+++ people.py
@@ -1,33 +1,34 @@
 from datetime import datetime
 
 def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S))
+    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
 
 # Data to serve with our API
 PEOPLE = {
-    "Igor": {
-        "fname": "Igor",
-        "lname": "Temchenko",
+    "Farrell": {
+        "fname": "Doug",
+        "lname": "Farrell",
         "timestamp": get_timestamp()
-        },
-    "Sergii": {
-        "fname": "Sergii"
-        "lname": "Temchenko"
+    },
+    "Brockman": {
+        "fname": "Kent",
+        "lname": "Brockman",
         "timestamp": get_timestamp()
-        }
-    "Oleksandr": {
-        "fname": "Oleksandr"
-        "lname": "Temchenko"
+    },
+    "Easter": {
+        "fname": "Bunny",
+        "lname": "Easter",
         "timestamp": get_timestamp()
-        }
+    }
 }
-# create a handler for our read (GET) people
+
+# Create a handler for our read (GET) people
 def read():
     """
-    This function responss to a request for /api/people
-    with the complete list of people
+    This function responds to a request for /api/people
+    with the complete lists of people
 
-    :return: sorted list of people
+    :return:        sorted list of people
     """
-    #create the list of people from our data
+    # Create the list of people from our data
     return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
