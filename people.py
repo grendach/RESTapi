e67eccb6d572ffc63c48d716c42b7d7396191c56
grendach <dmytro.grendach@nokia.com>
2018-06-17 16:13:09 +0200

    code refactoring #1

--- people.py
+++ people.py
@@ -19,22 +19,7 @@ def get_timestamp():
 
 # Data to serve with our API
 PEOPLE = {
-    "Farrell": {
-        "fname": "Doug",
-        "lname": "Farrell",
-        "timestamp": get_timestamp()
-    },
-    "Brockman": {
-        "fname": "Kent",
-        "lname": "Brockman",
-        "timestamp": get_timestamp()
-    },
-    "Easter": {
-        "fname": "Bunny",
-        "lname": "Easter",
-        "timestamp": get_timestamp()
     }
-}
 
 
 def read_all():
